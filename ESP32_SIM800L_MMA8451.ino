2f1118e704b5ad0bbd7685c793e516fd06009d54
Viet-Hung Vu <viethungvu@hotmail.com>
2019-12-05 19:44:48 +0100

    First full test (no sleep, no timer yet), messy

--- ESP32_SIM800L_MMA8451.ino
+++ ESP32_SIM800L_MMA8451.ino
@@ -3,14 +3,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_MMA8451.h>
+//#include <Adafruit_MMA8451.h>
 #include <ArduinoJson.h>
+#include "custom_src/MMA8451.h"
 
 // Your GPRS credentials (leave empty, if missing)
-const char apn[]      = ""; // Your APN
+const char apn[]      = "";//"iot.1nce.net";//""; // Your APN
 const char gprsUser[] = ""; // User
 const char gprsPass[] = ""; // Password
-const char simPIN[]   = "7526"; // SIM card PIN code, if any
+const char simPIN[]   = "";//"7526"; // SIM card PIN code, if any
 
 // Defines for TinyGSM library
 #define TINY_GSM_MODEM_SIM800
@@ -36,16 +37,18 @@ const char simPIN[]   = "7526"; // SIM card PIN code, if any
 #define STACK_SIZE 200
 
 // Defines for testing
-#define TEST_ACC  false
-#define TEST_SIM  true
-#define TEST_MQTT true
+#define TEST_ACC   false
+#define TEST_SIM   false
+#define TEST_MQTT  false
 #define TEST_TIMER false
-#define TEST_CORE false
-#define TEST_JSON 1
+#define TEST_CORE  false
+#define TEST_JSON  false
 
 TinyGsm       modem(SerialAT);
 TinyGsmClient client(modem);
 PubSubClient  mqtt(client);
+mma8451_t sensor;
+bool intFlag = false;
 
 static int32_t timerCounter = 0;
 
@@ -53,24 +56,30 @@ static int32_t timerCounter = 0;
 //StackType_t xStack[ STACK_SIZE ];
 xTaskHandle core0TaskHandle;
 
-static const char *broker = "mastap.net"; //"broker.hivemq.com"; //"mastap.net"; //"broker.hivemq.com";// 
+static const char *broker = "mastap.net";
+static const uint8_t INT_PIN = 14;
 static long lastReconnectAttempt = 0;
 static bool apnConnected = false;
 
-Adafruit_MMA8451 mma = Adafruit_MMA8451();
+//Adafruit_MMA8451 mma = Adafruit_MMA8451();
 hw_timer_t *timer    = NULL;
 
 void IRAM_ATTR onTimer() {
-  Serial.print("time: ");
-  Serial.print(millis() / 1000);
-  Serial.print("; counter: ");
+  Serial.print(F("time: "));
+  Serial.print(F(millis() / 1000));
+  Serial.print(F("; counter: "));
   Serial.println(timerCounter);
   timerCounter++;
 }
 
+void IRAM_ATTR accInterrupt() {
+  Serial.println(F("Interruptted"));
+  intFlag = true;
+}
+
 void core0Test(void *parameter) {
   for (;;) {
-    Serial.print("This task runs on core: ");
+    Serial.print(F("This task runs on core: "));
     Serial.println(xPortGetCoreID());
     digitalWrite(LED_PIN, HIGH);
     delay(1000);
@@ -80,55 +89,23 @@ void core0Test(void *parameter) {
 }
 
 bool mqttConnect() {
-  Serial.print("Connecting to mqtt broker: ");
+  Serial.print(F("Connecting to mqtt broker: "));
   Serial.println(broker);
 
   // Connect to MQTT Broker
   bool status = mqtt.connect("sim800l", "user", "mqtt");
-  //bool status = mqtt.connect("testtesttest");
+  //bool status = mqtt.connect("testtesttest"));
 
   if (status == false) {
-    Serial.println(" fail");
+    Serial.println(F(" fail"));
     return false;
   }
-  Serial.println(" success");
+  Serial.println(F(" success"));
   return mqtt.connected();
 }
 
-
-
-void setup () {
-  Serial.begin(115200);
-
-
-#if TEST_ACC
-  if (! mma.begin()) {
-    Serial.println("Couldnt start");
-    while (1);
-  }
-  Serial.println("MMA8451 found!");
-  mma.setRange(MMA8451_RANGE_2_G);
-
-  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
-  Serial.println("G");
-#endif
-
-#if TEST_TIMER
-  timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, onTimer, true);
-  timerAlarmWrite(timer, 2*1000000, true);
-  timerAlarmEnable(timer);
-#endif
-
-#if TEST_CORE
-  pinMode(LED_PIN, OUTPUT);
-  Serial.print("Stack: ");
-  xTaskCreatePinnedToCore(core0Test, "Core_0_Test", 1000, NULL, 1, &core0TaskHandle, 0);
-  Serial.println("Task created");
-#endif
-
-#if TEST_SIM
-  // Set-up modem reset, enable, power pins
+void setupModem() {
+    // Set-up modem reset, enable, power pins
   pinMode(MODEM_PWKEY, OUTPUT);
   pinMode(MODEM_RST, OUTPUT);
   pinMode(MODEM_POWER_ON, OUTPUT);
@@ -139,97 +116,75 @@ void setup () {
 
   SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
   delay(3000);
-  Serial.println("initializing modem");
+  Serial.println(F("initializing modem"));
   modem.restart(); // or modem.init();
 
   String modemInfo = modem.getModemInfo();
-  Serial.print("Modem: ");
+  Serial.print(F("Modem: "));
   Serial.println(modemInfo);
 
   // Unlock your SIM card with a PIN if needed
   if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
-    Serial.println("Unlocking sim card");
+    Serial.println(F("Unlocking sim card"));
     bool ret = modem.simUnlock(simPIN);
     delay(1000);
-    Serial.print("SIM status: ");
+    Serial.print(F("SIM status: "));
     uint8_t simStatus = modem.getSimStatus();
     Serial.println(ret);
   }
-  Serial.print("SIM status: ");
+  Serial.print(F("SIM status: "));
   uint8_t simStatus = modem.getSimStatus();
   Serial.println(simStatus);
   String ccid = modem.getSimCCID();
-  Serial.print("SIM CCID: ");
+  Serial.print(F("SIM CCID: "));
   Serial.println(ccid);
   String emei = modem.getIMEI();
-  Serial.print("SIM IMEI: ");
+  Serial.print(F("SIM IMEI: "));
   Serial.println(emei);
   String cop = modem.getOperator();
-  Serial.print("SIM Operator: ");
+  Serial.print(F("SIM Operator: "));
   Serial.println(cop);
 
-  Serial.print("Waiting for network...");
+  Serial.print(F("Waiting for network..."));
   if (!modem.waitForNetwork(240000L)) {
-    Serial.println(" fail");
+    Serial.println(F(" fail"));
     delay(10000);
     return;
   }
-  Serial.println(" OK");
+  Serial.println(F(" OK"));
 
   if (modem.isNetworkConnected()) {
-    Serial.println("Network connected");
+    Serial.println(F("Network connected"));
   }
 
   Serial.print(F("Connecting to APN: "));
   Serial.print(apn);
   if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    Serial.println(" fail");
+    Serial.println(F(" fail"));
     delay(10000);
     return;
   }
 
   if (modem.isGprsConnected()) {
-    Serial.println("GPRS connected");
+    Serial.println(F("GPRS connected"));
   }
-#endif
-
-#if TEST_MQTT
-  mqtt.setServer(broker, 1883);
-#endif
 }
 
-void loop() {
-#if TEST_ACC
-  // Read the 'raw' data in 14-bit counts
-  mma.read();
-  Serial.print("X:\t"); Serial.print(mma.x); 
-  Serial.print("\tY:\t"); Serial.print(mma.y); 
-  Serial.print("\tZ:\t"); Serial.print(mma.z); 
-  Serial.println();
+void setupSensor() {
+  sensor.i2cAddress = MMA8451_DEFAULT_ADDRESS;
+  bool ret = MMA8451_begin(&sensor);
+  if (!ret) {
+    Serial.println(F("Init failed"));
+    while(1);
+  }
+  MMA8451_enableInterrupt(&sensor, 700, 300, true);
+}
 
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
-  mma.getEvent(&event);
-
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: \t"); Serial.print(event.acceleration.x); Serial.print("\t");
-  Serial.print("Y: \t"); Serial.print(event.acceleration.y); Serial.print("\t");
-  Serial.print("Z: \t"); Serial.print(event.acceleration.z); Serial.print("\t");
-  Serial.println("m/s^2 ");
-
-  Serial.println();
-  delay(500);
-#endif
-
-#if TEST_SIM
-
-#endif
-
-#if TEST_MQTT
+void setupMQTT() {
   while (!mqtt.connected() && modem.isGprsConnected()) {
     // Reconnect every 10 seconds
     unsigned long t = millis();
-    Serial.println(t, DEC);
+    //Serial.println(t, DEC);
     if (t - lastReconnectAttempt > 10000L) {
       lastReconnectAttempt = t;
       if (mqttConnect()) {
@@ -237,41 +192,155 @@ void loop() {
       }
       else {
         lastReconnectAttempt = 0;
-        Serial.println("Retrying..");
+        Serial.println(F("Retrying.."));
         Serial.println(t, DEC);
         Serial.println(lastReconnectAttempt, DEC);
       }
     }
     delay(100);
   }
-  Serial.println("mqtt connected");
-  const char message[] = "Hello World";
 
-  if (TEST_JSON) {
-    Serial.println("Jsonizing..");
-    const int capacity=JSON_OBJECT_SIZE(3);
-    StaticJsonDocument<capacity>doc;
-    char buffer[128];
-    doc["value"]=42;
-    doc["lat"]=48.748010;
-    doc["lon"]=2.293491;
-    size_t n = serializeJson(doc, buffer);
-    Serial.print("Size of payload: "); Serial.println(n);
-    bool ret = mqtt.publish("ngd/demo/lv002/data", buffer, n);
-    Serial.print("Publish ");
-    Serial.println(ret ? " OK" : " Failed");
-    delay(30000);
-  }
-  else 
-  {
-    bool ret = mqtt.publish("ngd/demo/lv001/data", message, sizeof(message));
-    Serial.print("Publish ");
-    Serial.println(ret ? " OK" : " Failed");
-    delay(60000);
-  }
+  Serial.println();
 
-  return;
+  Serial.println(F("mqtt connected"));
+}
 
-  mqtt.loop();
+void setup () {
+  Serial.begin(115200);
+  Serial.println(F("Here we go"));
+
+#if TEST_TIMER
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, onTimer, true);
+  timerAlarmWrite(timer, 2*1000000, true);
+  timerAlarmEnable(timer);
 #endif
+
+#if TEST_CORE
+  pinMode(LED_PIN, OUTPUT);
+  Serial.print(F("Stack: "));
+  xTaskCreatePinnedToCore(core0Test, "Core_0_Test", 1000, NULL, 1, &core0TaskHandle, 0);
+  Serial.println(F("Task created"));
+#endif
+  setupSensor();
+  pinMode(INT_PIN, INPUT_PULLDOWN);
+  attachInterrupt(INT_PIN, accInterrupt, RISING);
+  setupModem();
+  mqtt.setServer(broker, 1883);
+}
+
+void loop() {
+  //attachInterrupt(INT_PIN, accInterrupt, RISING);
+
+  // MMA8451_readData(&sensor);
+  // Serial.print(F("x: ")); Serial.print(F((float)sensor.data.x/2048); Serial.print(F("; "));
+  // Serial.print(F("y: ")); Serial.print(F((float)sensor.data.y/2048); Serial.print(F("; "));
+  // Serial.print(F("z: ")); Serial.print(F((float)sensor.data.z/2048); Serial.print(F("; "));
+  // Serial.println();
+
+  // uint8_t intSource = MMA8451_getInterruptSource(&sensor);
+  // Serial.print(F("int src: ")); Serial.println(intSource, BIN);
+  Serial.print(F("."));
+  if (intFlag) {
+    intFlag = false;
+    detachInterrupt(INT_PIN);
+    
+    Serial.println();
+    delay(100);
+
+    sensors_event_t event;
+
+    const uint16_t BUFFER_SIZE = 1024;
+
+    uint32_t heapSize = esp_get_free_heap_size();
+    Serial.print(F("Free heap size: ")); Serial.println(heapSize);
+
+    const size_t capacity = 4*JSON_ARRAY_SIZE(BUFFER_SIZE) + JSON_OBJECT_SIZE(5) + 50;
+    DynamicJsonDocument doc(capacity);
+    JsonArray x = doc.createNestedArray("xba");
+    JsonArray y = doc.createNestedArray("yba");
+    JsonArray z = doc.createNestedArray("zba");
+    JsonArray t = doc.createNestedArray("tba");
+    doc["timestamp"] = modem.getGSMDateTime(DATE_TIME);
+    Serial.print(F("timestamp: ")); Serial.println(modem.getGSMDateTime(DATE_TIME));
+
+    uint32_t startTime = millis();
+    uint32_t newTime, oldTime;
+    newTime = oldTime = startTime;
+
+    Serial.print(F("Start time: ")); 
+    Serial.println(startTime);
+    for (uint16_t index = 0; index  < BUFFER_SIZE; index++) {
+      while (newTime - oldTime < 10) {
+        newTime = millis();
+      }
+      oldTime = newTime;
+    
+      MMA8451_readData(&sensor);
+      x.add((float)sensor.data.x/2048);
+      y.add((float)sensor.data.y/2048);
+      z.add((float)sensor.data.z/2048);
+      t.add(index);
+
+      if (index % 100 == 0) {
+        Serial.print(F("Index: ")); Serial.print(index);
+        Serial.print(F(" at: ")); Serial.println(newTime);
+      }
+    }
+    Serial.print(F("Stop time: ")); 
+    Serial.println(newTime);
+
+    heapSize = esp_get_free_heap_size();
+    Serial.print(F("Free heap size: ")); Serial.println(heapSize);
+    Serial.print(F("JSON cap: ")); Serial.println(capacity);
+    Serial.print(F("JSON size :")); Serial.println(doc.size());
+
+    char *buffer = (char *)malloc(capacity);
+    uint16_t n = measureMsgPack(doc);
+    uint16_t bytesSerialized = serializeMsgPack(doc, buffer, n + 1);
+    Serial.print(F("Size of payload: ")); Serial.println(n);
+    Serial.print(F("Bytes serialized: ")); Serial.println(bytesSerialized);
+
+    heapSize = esp_get_free_heap_size();
+    Serial.print(F("Free heap size: ")); Serial.println(heapSize);
+
+    setupMQTT();
+
+    Serial.println(F("Publishing.."));
+    bool ret;
+    ret = mqtt.beginPublish("ngd/demo/gprs001/data", n, false); 
+    Serial.print(F("ret")); Serial.println(ret ? " OK" : " Failed");
+    uint16_t bytesRemaining = n;
+    uint8_t *pointerToBuffer = (uint8_t *)buffer;
+    Serial.print(F("0. bytes remaining: ")); Serial.println(bytesRemaining);
+    while (bytesRemaining) {
+      uint16_t bytesToWrite = (bytesRemaining > n % 512 ? 512 : n % 512);
+      Serial.print(F("bytes to write:")); Serial.println(bytesToWrite);
+      uint16_t wrote = mqtt.write(pointerToBuffer, bytesToWrite);
+      Serial.print(F("wrote ")); Serial.println(wrote);
+      bytesRemaining -= wrote;
+      pointerToBuffer += wrote;
+      Serial.print(F("bytes remaining: ")); Serial.println(bytesRemaining);
+      //mqtt.loop();
+    }
+    ret = mqtt.endPublish(); 
+    Serial.print(F("ret")); Serial.println(ret ? " OK" : " Failed");
+
+    Serial.print(F("ret")); Serial.println(ret ? " OK" : " Failed");
+    Serial.println(F("Done publishing"));
+
+    pointerToBuffer = NULL;
+    free(buffer);
+    buffer = NULL;
+    
+    Serial.print(F("MQTT connected: ")); Serial.println(mqtt.connected() ? "YES" : "NO");
+    attachInterrupt(INT_PIN, accInterrupt, RISING);
+    Serial.print(F("FF_MT_SRC: ")); Serial.println(MMA8451_getMotionSource(&sensor), BIN);
+    Serial.print(F("INT_SRC: "));Serial.println(MMA8451_getInterruptSource(&sensor), BIN);
+    delay(100);
+    Serial.print(F("FF_MT_SRC: ")); Serial.println(MMA8451_getMotionSource(&sensor), BIN);
+    Serial.print(F("INT_SRC: "));Serial.println(MMA8451_getInterruptSource(&sensor), BIN);
+    //delay(20000);
+  }
+  delay(100); 
 }
