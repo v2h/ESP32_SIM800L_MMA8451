f364e73f10958746f5f04f29bf8d8b7ffb3ac2d6
islandmonkey <moritz.pruem@hochschule-rhein-waal.de>
2020-02-25 15:29:06 +0100

    implemented new MMA8451 library

--- ESP32_SIM800L_MMA8451.ino
+++ ESP32_SIM800L_MMA8451.ino
@@ -1,27 +1,28 @@
 /*
-//// MMA8451 ////
-Sampling Frequency: 800Hz
-See: MMA8451.cpp
-Function: MMA8451_enableInterrupt()
-Line: 94 , MMA8451_writeReg8(sensor, MMA8451_REG_CTRL_REG1, 0x01 | 0x04); // active, 800Hz, low noise
-Note: Frequency only gets set when MMA8451_enableInterrupt() is called ????
+  //// MMA8451 ////
+  Sampling Frequency: 800Hz
+  See: MMA8451.cpp
+  Function: MMA8451_enableInterrupt()
+  Line: 94 , MMA8451_writeReg8(sensor, MMA8451_REG_CTRL_REG1, 0x01 | 0x04); // active, 800Hz, low noise
+  Note: Frequency only gets set when MMA8451_enableInterrupt() is called ????
 
-//// OPEN QUESTIONS ////
--- Can the measurement interval frequency be changed to 0 through Node Red's command ? What happens then?
+  //// OPEN QUESTIONS ////
+  -- Can the measurement interval frequency be changed to 0 through Node Red's command ? What happens then?
 
--- detachInterrupt when doing mqtt transmission and other critical tasks!!!
+  -- detachInterrupt when doing mqtt transmission and other critical tasks!!!
 
--- Replace Delays ....
+  -- Replace Delays ....
 */
 
 // https://github.com/Xinyuan-LilyGO/TTGO-T-Call
 // https://pubsubclient.knolleary.net/api.html for pubsubclient
 
 #include <Arduino.h>
-#include <Wire.h>
-#include "custom_src/MMA8451.h"
-#include "custom_src/mpack.h"
+/*#include <Wire.h>
+  #include "custom_src/MMA8451.h"*/
+#include "custom_src/mpack.h"*/
 #include <FastLED.h>
+#include "custom_src/my_MMA845XQ_V002.h"
 
 //#define TGO_BOARD
 
@@ -48,8 +49,8 @@ const char simPIN[]   = "";//"7526"; // SIM card PIN code, if any
 
 // ???????? Needed for the sim800 version??
 /*// For the voltage regulator chip thingy
-#define I2C_SDA 21
-#define I2C_SCL 22*/
+  #define I2C_SDA 21
+  #define I2C_SCL 22*/
 
 #define INT_PIN   15 // Interrupt of the MMA8451
 #define LED_DATA_PIN 32
@@ -66,8 +67,8 @@ const char simPIN[]   = "";//"7526"; // SIM card PIN code, if any
 
 TinyGsm       g_modem(SerialAT);
 TinyGsmClient g_client(g_modem);
-PubSubClient  g_mqtt(g_client);
-mma8451_t     g_sensor;
+PubSubClient  g_mqtt(g_client);//mma8451_t     g_sensor;
+MMA8451Q accel(0x1D);
 CRGB leds[NUM_LEDS];
 
 hw_timer_t *g_timer    = NULL;
@@ -105,25 +106,26 @@ void IRAM_ATTR accelerometerISR();
 
 void setup() {
   startWatchDog();
+  Wire.begin();
   Serial.begin(115200);
   Serial.println(F("LumaVibe V2 Starting ..."));
-/*#ifdef TGO_BOARD
-  #define MODEM_RST            5
-  #define MODEM_PWKEY          4
-  #define MODEM_POWER_ON       23
-  // Set-up modem reset, enable, power pins
-  pinMode(MODEM_PWKEY, OUTPUT);
-  pinMode(MODEM_RST, OUTPUT);
-  pinMode(MODEM_POWER_ON, OUTPUT);
+  /*#ifdef TGO_BOARD
+    #define MODEM_RST            5
+    #define MODEM_PWKEY          4
+    #define MODEM_POWER_ON       23
+    // Set-up modem reset, enable, power pins
+    pinMode(MODEM_PWKEY, OUTPUT);
+    pinMode(MODEM_RST, OUTPUT);
+    pinMode(MODEM_POWER_ON, OUTPUT);
 
-  digitalWrite(MODEM_PWKEY, LOW);
-  digitalWrite(MODEM_RST, HIGH);
-  digitalWrite(MODEM_POWER_ON, HIGH);
+    digitalWrite(MODEM_PWKEY, LOW);
+    digitalWrite(MODEM_RST, HIGH);
+    digitalWrite(MODEM_POWER_ON, HIGH);
 
-  Wire.begin(I2C_SDA, I2C_SCL);
-  bool   isOk = setPowerBoostKeepOn(1);
-  Serial.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
-#endif*/
+    Wire.begin(I2C_SDA, I2C_SCL);
+    bool   isOk = setPowerBoostKeepOn(1);
+    Serial.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
+    #endif*/
   FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, NUM_LEDS);
   leds[0] = CRGB::Red;
   FastLED.show();
@@ -149,44 +151,49 @@ void setup() {
 
 
 void loop() {
-  
+
   /// double check the WDT is runnning??? Is there a way?
 
   if (g_accelerometerInterruptFlag || g_timerFlag) {
     startWatchDog();
-  
-   // detachInterrupt and endTimer as fast as possible. Makes more sense right here...
-    //detachInterrupt(INT_PIN); 
+
+    // detachInterrupt and endTimer as fast as possible. Makes more sense right here...
+    //detachInterrupt(INT_PIN);
     //endTimer();
-    
-    
-    
-//    // Reading the FF_MT_SRC Register Clears the Interrupt on the MMA8451
-//    Serial.print(F("FF_MT_SRC: ")); Serial.println(MMA8451_getMotionSource(&g_sensor), BIN);
-//    //delay(100);
-//    Serial.print(F("INT_SRC: "));Serial.println(MMA8451_getInterruptSource(&g_sensor), BIN);
+
+
+
+    //    // Reading the FF_MT_SRC Register Clears the Interrupt on the MMA8451
+    //    Serial.print(F("FF_MT_SRC: ")); Serial.println(MMA8451_getMotionSource(&g_sensor), BIN);
+    //    //delay(100);
+    //    Serial.print(F("INT_SRC: "));Serial.println(MMA8451_getInterruptSource(&g_sensor), BIN);
 
     /*if (g_accelerometerInterruptFlag) {
       Serial.println(F("\nacceleration interrupt"));
       g_accelerometerInterruptFlag = false;
-    }
-    if (g_timerFlag) {
+      }
+      if (g_timerFlag) {
       Serial.println(F("\ntimer interrupt"));
       g_timerFlag = false;
-    }*/
+      }*/
 
     leds[0] = CRGB::Blue;
     FastLED.show();
+
 
     uint16_t threshold_temp = g_threshold;
     uint16_t duration_temp = g_duration;
 
     if (g_accelerometerInterruptFlag) {
-      Serial.println(F("\nacceleration interrupt"));
+      uint8_t mo_src = accel.getMotionSource(); // Read to clear EA flag
+      uint8_t tr_src = accel.getTransientSource(); // Read to clear EA flag
+      uint8_t res = accel._read_register(accel.INT_SOURCE);
+      accel._PrintHex8(&res, 1);  Serial.print("\t");
+      Serial.println(F("\nAcceleration interrupt"));
       g_accelerometerInterruptFlag = false;
     }
     if (g_timerFlag) {
-      Serial.println(F("\ntimer interrupt"));
+      Serial.println(F("\nTimer interrupt"));
       g_timerFlag = false;
     }
 
@@ -206,10 +213,14 @@ void loop() {
     for (uint16_t index = 0; index  < BUFFER_SIZE; index++) {
       uint32_t startTime = millis();
       while (millis() - startTime < MEASUREMENT_PER_MS);
-      MMA8451_readData(&g_sensor);
-      data->x[index] = (g_sensor.data.x);
-      data->y[index] = (g_sensor.data.y);
-      data->z[index] = (g_sensor.data.z);
+      accel.update();
+      //MMA8451_readData(&g_sensor);
+      //data->x[index] = (g_sensor.data.x);
+      //data->y[index] = (g_sensor.data.y);
+      //data->z[index] = (g_sensor.data.z);
+      data->x[index] = (accel._xi);
+      data->y[index] = (accel._yi);
+      data->z[index] = (accel._zi);
 
       if (index % 100 == 0) {
         Serial.print(F("Index: ")); Serial.print(index);
@@ -225,7 +236,7 @@ void loop() {
     String location = g_modem.getGsmLocation();
     Serial.print(F("location: ")); Serial.println(location);
     uint8_t index = location.lastIndexOf(',', 19);
-    String timeStamp = location.substring(index+1);
+    String timeStamp = location.substring(index + 1);
     timeStamp.replace('/', '-');
     timeStamp.replace(',', 'T');
     Serial.print(F("timestamp: ")); Serial.println(timeStamp);
@@ -248,19 +259,19 @@ void loop() {
     mpack_write_tag(&writer, mpack_tag_make_array(BUFFER_SIZE));
     Serial.println(F("check 2"));
     for (uint16_t i = 0; i < BUFFER_SIZE; i++) {
-        mpack_write_i16(&writer, data->x[i]);
+      mpack_write_i16(&writer, data->x[i]);
     }
     Serial.println(F("check 3"));
     mpack_write_cstr(&writer, "y-accel");
     mpack_write_tag(&writer, mpack_tag_make_array(BUFFER_SIZE));
     for (uint16_t i = 0; i < BUFFER_SIZE; i++) {
-        mpack_write_i16(&writer, data->y[i]);
+      mpack_write_i16(&writer, data->y[i]);
     }
     Serial.println(F("check 4"));
     mpack_write_cstr(&writer, "z-accel");
     mpack_write_tag(&writer, mpack_tag_make_array(BUFFER_SIZE));
     for (uint16_t i = 0; i < BUFFER_SIZE; i++) {
-        mpack_write_i16(&writer, data->z[i]);
+      mpack_write_i16(&writer, data->z[i]);
     }
     Serial.println(F("check 5"));
 
@@ -294,19 +305,19 @@ void loop() {
     g_mqtt.disconnect();
 
     Serial.print(F("MQTT connected: ")); Serial.println(g_mqtt.connected() ? "YES" : "NO");
-    Serial.print(F("FF_MT_SRC: ")); Serial.println(MMA8451_getMotionSource(&g_sensor), BIN);
-    delay(100);
-    Serial.print(F("INT_SRC: "));Serial.println(MMA8451_getInterruptSource(&g_sensor), BIN);
+    //Serial.print(F("FF_MT_SRC: ")); Serial.println(MMA8451_getMotionSource(&g_sensor), BIN);
+    //delay(100);
+    //Serial.print(F("INT_SRC: ")); Serial.println(MMA8451_getInterruptSource(&g_sensor), BIN);
 
-    Serial.println(F("Reconfiguring interrupt"));
-    MMA8451_enableInterrupt(&g_sensor, g_threshold, g_duration, true, true);
-    Serial.println(F("Interrupt reconfigured"));
+    //Serial.println(F("Reconfiguring interrupt"));
+    //MA8451_enableInterrupt(&g_sensor, g_threshold, g_duration, true, true);
+    //Serial.println(F("Interrupt reconfigured"));
 
     /*if ((g_threshold != threshold_temp) || (g_duration != duration_temp)) {
       Serial.println(F("Reconfiguring interrupt"));
       MMA8451_enableInterrupt(&g_sensor, g_threshold, g_duration, true, true);
       Serial.println(F("Interrupt reconfigured"));
-    }*/
+      }*/
 
     endWatchDog();
 
@@ -329,12 +340,12 @@ void loop() {
 
 void updateSerial()
 {
-//  delay(500);
+  //  delay(500);
   while (Serial.available())
   {
     SerialAT.write(Serial.read());//Forward what Serial received to Software Serial Port
   }
-  while(SerialAT.available())
+  while (SerialAT.available())
   {
     Serial.write(SerialAT.read());//Forward what Software Serial received to Serial Port
   }
@@ -419,7 +430,7 @@ bool mqttConnect() {
 }
 
 void setupMQTT() {
-  while(!g_modem.isGprsConnected()) {
+  while (!g_modem.isGprsConnected()) {
     Serial.println(F("connecting to gprs"));
     g_modem.gprsConnect(apn, gprsUser, gprsPass);
     delay(1000);
@@ -445,19 +456,35 @@ void setupMQTT() {
 
 void setupSensor() {
   Serial.println(F("Initializing MMA8451"));
-  g_sensor.i2cAddress = MMA8451_DEFAULT_ADDRESS;
-  bool ret = MMA8451_begin(&g_sensor);
-  if (!ret) {
-    Serial.println(F("Init failed"));
-    while(1);
-  }
-  //MMA8451_getMotionSource(&g_sensor) // Clear previous interrupt in case there was one
-  MMA8451_enableInterrupt(&g_sensor, g_threshold, g_duration, true, true);
+  //  g_sensor.i2cAddress = MMA8451_DEFAULT_ADDRESS;
+  //  bool ret = MMA8451_begin(&g_sensor);
+  //  if (!ret) {
+  //    Serial.println(F("Init failed"));
+  //    while(1);
+  //  }
+  //  //MMA8451_getMotionSource(&g_sensor) // Clear previous interrupt in case there was one
+  //  MMA8451_enableInterrupt(&g_sensor, g_threshold, g_duration, true, true);
+
+  accel.SWreset(); // RESET THE REGS TO FACTORY DEFAULT FIRST!
+  accel.setCommonParameters(accel.RANGE_2G, accel.RES_MAX, accel.LN_OFF, accel.DR_100, accel.OS_NORMAL, accel.HPF_OFF);
+
+  // USE TRANSIENT DETECTION!
+  accel.setTransientDetection();
+  accel.setTransientThresholdG(0.07, false);
+  accel.setTransientDebounceCounter(2);
+  accel.setHPFilterCutOff(3);
+  //// Using INT_PIN 2 = INT1 - DO NOT USE WITH Vibe V2.00 !!!
+  //accel.setInterrupt(accel.INT_EN_TRANS, accel.INT1, true); // Which event should raise an interrupt, and to which MMA8451Q pin is it routed (pins INT1 and INT2)
+  // Using INT_PIN 15 = INT2
+  accel.setInterrupt(accel.INT_EN_TRANS, accel.INT2, true); // Which event should raise an interrupt, and to which MMA8451Q pin is it routed (pins INT1 and INT2)
+
+  uint8_t mo_src = accel.getMotionSource(); // Read to clear EA flag
+  uint8_t tr_src = accel.getTransientSource(); // Read to clear EA flag
 }
 
 void IRAM_ATTR accelerometerISR() {
-  MMA8451_getMotionSource(&g_sensor);
   detachInterrupt(INT_PIN);
+  //MMA8451_getMotionSource(&g_sensor);
   g_accelerometerInterruptFlag = true;
   Serial.println(F("\nInterruptted"));
 }
@@ -532,11 +559,11 @@ void publishMQTT(const uint8_t *buffer, uint32_t bytesTotal, uint16_t bytesPerWr
 
 /*#ifdef TGO_BOARD
 
-#define IP5306_ADDR          0x75
-#define IP5306_REG_SYS_CTL0  0x00
+  #define IP5306_ADDR          0x75
+  #define IP5306_REG_SYS_CTL0  0x00
 
-bool setPowerBoostKeepOn(int en)
-{
+  bool setPowerBoostKeepOn(int en)
+  {
   Wire.beginTransmission(IP5306_ADDR);
   Wire.write(IP5306_REG_SYS_CTL0);
   if (en) {
@@ -545,9 +572,9 @@ bool setPowerBoostKeepOn(int en)
     Wire.write(0x35); // 0x37 is default reg value
   }
   return Wire.endTransmission() == 0;
-}
+  }
 
-#endif*/
+  #endif*/
 
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
@@ -576,13 +603,13 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
   duration = mpack_expect_u16(&reader);
 
   /*
-  mpack_expect_cstr_match(&reader, "deadtime");
-  uint16_t deadtime;
-  deadtime = mpack_expect_u16(&reader);
+    mpack_expect_cstr_match(&reader, "deadtime");
+    uint16_t deadtime;
+    deadtime = mpack_expect_u16(&reader);
 
-  mpack_expect_cstr_match(&reader, "period");
-  uint16_t period;
-  period = mpack_expect_u16(&reader);
+    mpack_expect_cstr_match(&reader, "period");
+    uint16_t period;
+    period = mpack_expect_u16(&reader);
   */
 
   mpack_expect_cstr_match(&reader, "_msgid");
