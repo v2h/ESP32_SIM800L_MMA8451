27d7a48278d147354791fd1779b6848aab7b6465
Hung Vu <viethungvu@hotmail.com>
2020-03-23 04:18:52 +0100

    Add macros for debugging

---
+++ debug_macros.h
@@ -0,0 +1,26 @@
+#ifndef  DEBUG_MACROS_H
+#define DEBUG_MACROS_H
+
+#if DEBUG_MACROS_ENABLE
+#define PRINTS(s) do {  \
+    Serial.print(F(s)); \
+  } while (0)           \
+
+#define PRINT(s,v) do { \
+    Serial.print(F(s)); \
+    Serial.print(v);    \
+  } while(0)            \
+
+#define PRINTHEX(s,v) do { \
+    Serial.print(F(s));    \
+    Serial.print(F("0x")); \
+    Serial.print(v, HEX);  \
+  } while (0)
+#else
+#define PRINTS(s)
+#define PRINT(s,v)
+#define PRINTHEX(s,v)
+
+#endif // DEBUG_MACROS_ENABLE
+
+#endif // DEBUG_MACROS_H
