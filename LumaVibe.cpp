094f30d0bb641237eae83e945eed1fd770bb12a0
Hung Vu <viethungvu@hotmail.com>
2020-03-23 15:09:29 +0100

    Update functions / Refactor

--- LumaVibe.cpp
+++ LumaVibe.cpp
@@ -3,6 +3,8 @@
 #define FCLK_DIVIDER          80
 #define WATCHDOG_TIMER_NUMBER 1
 #define SLEEP_TIMER_NUMBER    0
+#define PACKER_CAPACITY       40000
+#define BYTES_PER_WRITE       512
 
 #define SerialAT  Serial1
 #define SerialUSB Serial
@@ -10,7 +12,26 @@
 #define MODEM_TX 27
 #define MODEM_RX 26
 
-LumaVibe::LumaVibe() : _accel(0x1D), _modem(SerialAT), _client(_modem), _mqtt(_client) {}
+static const struct {
+  char *timestamp    = "timestamp";
+  char *moduleID     = "moduleID";
+  char *moduletype   = "moduletype";
+  char *msgtype      = "msgtype";
+  char *format       = "format";
+  char *freq         = "freq";
+  char *numberOfMeas = "numberOfMeas";
+  char *x_accel      = "x-accel";
+  char *y_accel      = "y-accel";
+  char *z_accel      = "z-accel";
+} StringToPack;
+
+// Default Initializer
+LumaVibe::LumaVibe() : 
+  _accel(0x1D), 
+  _modem(SerialAT), 
+  _client(_modem), 
+  _mqtt(_client) 
+  {}
 
 // Copy parameters only, doesn't really do anything hardware-wise
 LumaVibe::ERROR LumaVibe::init(LumaVibe::Parameters_t params) {
@@ -64,6 +85,9 @@ LumaVibe::ERROR LumaVibe::begin () {
 
 //
 LumaVibe::ERROR LumaVibe::measure() {
+  if (_accelBuffer.isBufferAllocated) {
+    clearMeasurementData();
+  }
   _accelBuffer.data = (decltype(_accelBuffer.data))malloc(_params.samplesPerMeasurement * sizeof(*(_accelBuffer.data)));
   if (NULL == _accelBuffer.data) {
     return ERROR_NOT_ENOUGH_MEMORY;
@@ -87,9 +111,109 @@ LumaVibe::ERROR LumaVibe::measure() {
   return ERROR_NONE;
 }
 
-//////////////////////
-/* Private methods */
-/////////////////////
+//
+LumaVibe::ERROR LumaVibe::packData(uint32_t *bytesPacked) {
+  _packBuffer = (char *)malloc(PACKER_CAPACITY);
+  if (NULL == _packBuffer) {
+    return ERROR_NOT_ENOUGH_MEMORY;
+  }
+  mpack_writer_init(&_writer, _packBuffer, PACKER_CAPACITY);
+  mpack_start_map(&_writer, 10);
+  String timeStamp;
+  getTimestampFromNetwork(timeStamp);
+  // TODO: refactor this shit, from here..
+  mpack_write_cstr(&_writer, StringToPack.timestamp); 
+  mpack_write_cstr(&_writer, timeStamp.c_str());
+  mpack_write_cstr(&_writer, StringToPack.moduleID); 
+  mpack_write_cstr(&_writer, _params.moduleID);
+  mpack_write_cstr(&_writer, StringToPack.moduletype);
+  mpack_write_cstr(&_writer, _params.moduleType);
+  mpack_write_cstr(&_writer, StringToPack.msgtype);
+  mpack_write_u8(&_writer, _params.msgType);
+  mpack_write_cstr(&_writer, StringToPack.format);
+  mpack_write_cstr(&_writer, _params.format);
+  mpack_write_cstr(&_writer, StringToPack.freq);
+  mpack_write_u16(&_writer, _params.frequency);
+  mpack_write_cstr(&_writer, StringToPack.numberOfMeas);
+  mpack_write_u16(&_writer, _params.samplesPerMeasurement);
+  // ..to here
+  const char * entries[] = {StringToPack.x_accel, StringToPack.y_accel, StringToPack.z_accel};
+  packArray(&_writer, entries, _params.samplesPerMeasurement);
+
+  *bytesPacked = mpack_writer_buffer_used(&_writer); // for debugging, add later
+
+  keepAlive();
+  return ERROR_NONE;
+}
+
+//
+LumaVibe::ERROR LumaVibe::publishData(uint32_t bytesToPublish, uint16_t bytesPerWrite) {
+  if (!_modem.isGprsConnected()) {
+    if (!_modem.gprsConnect("", "", ""));
+      return ERROR_MODEM_GPRS;
+  }
+  if (!_mqtt.connected()) {
+    if (!_mqtt.connect("sim800l", "user", "mqtt"))
+      return ERROR_MQTT;
+  }
+  keepAlive();
+  if (_mqtt.beginPublish(_params.publishTopic, bytesToPublish, false)) {
+    return ERROR_PUBLISH_BEGIN_FAIL;
+  }
+  uint8_t *pointerToBuffer = (uint8_t *)_packBuffer;
+  while (bytesToPublish) {
+    uint16_t bytesToWrite = (bytesToPublish > bytesToPublish % bytesPerWrite ? bytesPerWrite : bytesToPublish % bytesPerWrite);
+    uint16_t bytesWritten = _mqtt.write(pointerToBuffer, bytesToWrite);
+    bytesToPublish -= bytesWritten;
+    pointerToBuffer += bytesWritten;
+    _mqtt.loop();
+    yield();
+  }
+  if (!_mqtt.endPublish()) {
+    return ERROR_PUBLISH_END_FAIL;
+  }
+
+  free(_packBuffer);
+  _packBuffer = NULL;
+  return ERROR_NONE;
+}
+
+//
+void LumaVibe::getCommandsFromServer(MQTT_CALLBACK_SIGNATURE) {
+  _mqtt.setCallback(callback);
+  _mqtt.subscribe(_params.subscribeTopic);
+  delay(2000);
+  _mqtt.loop();
+  _mqtt.loop();
+  _mqtt.disconnect();
+  keepAlive();
+}
+
+//
+void LumaVibe::clearAccelInterrupt() {
+  _accel._read_register(MMA8451Q::INT_SOURCE);
+  _accel.getTransientSource();
+}
+
+// 
+void LumaVibe::dumpSimInfo() {
+  Serial.print(F("SIM status: "));
+  uint8_t simStatus = _modem.getSimStatus();
+  Serial.println(simStatus);
+  String ccid = _modem.getSimCCID();
+  Serial.print(F("SIM CCID: "));
+  Serial.println(ccid);
+  String emei = _modem.getIMEI();
+  Serial.print(F("SIM IMEI: "));
+  Serial.println(emei);
+  String cop = _modem.getOperator();
+  Serial.print(F("SIM Operator: "));
+  Serial.println(cop);
+}
+
+//////////////////////////////////////////////////////////////////
+///////////////////////* Private methods *///////////////////////
+/////////////////////////////////////////////////////////////////
 
 //
 LumaVibe::ERROR LumaVibe::enableTimer(hw_timer_t *timer, uint8_t timerNumber, uint64_t timer_ms, void (*timerISR)(void)) {
@@ -116,6 +240,8 @@ void LumaVibe::keepAlive() {
 void LumaVibe::clearMeasurementData() {
   if (_accelBuffer.isBufferAllocated) {
     free(_accelBuffer.data);
+    _accelBuffer.data = NULL;
+    _accelBuffer.isBufferAllocated = false;
   }
 }
 
@@ -130,8 +256,29 @@ LumaVibe::ERROR LumaVibe::setupModem() {
   if (_modem.gprsConnect("","","")) // .gprsConnect(apn, gprsUser, gprsPass)
     return ERROR_MODEM_GPRS;
   
-  // TODO: function to dump network and sim card information
-
   keepAlive();
   return ERROR_NONE;
+}
+
+//
+LumaVibe::ERROR LumaVibe::getTimestampFromNetwork(String &timeStamp) {
+  //TODO: Get rid of Arduino's String, handle possible errors
+  String location = _modem.getGsmLocation();
+  uint8_t index = location.lastIndexOf(',', 19);
+  timeStamp = location.substring(index + 1);
+  timeStamp.replace('/', '-');
+  timeStamp.replace(',', 'T');
+
+  return ERROR_NONE;
+}
+
+//
+void LumaVibe::packArray(mpack_writer_t *writer, const char *entryNames[3], const uint16_t length) {
+  for (uint8_t i = 0; i < 3; i++) {
+    mpack_write_cstr(writer, entryNames[i]);
+    mpack_write_tag(writer, mpack_tag_make_array(length));
+    for (uint16_t j = 0; j < length; i++) {
+      mpack_write_i16(writer, _accelBuffer.data[j].v[i]);
+    }
+  }
 }
--- LumaVibe.h
+++ LumaVibe.h
@@ -10,7 +10,8 @@
 #define TINY_GSM_USE_WIFI     false
 #include <TinyGsmClient.h> // https://github.com/vshymanskyy/TinyGSM
 
-#include <PubSubClient.h>  // https://github.com/knolleary/pubsubclient
+#include "PubSubClient.h"  // https://github.com/knolleary/pubsubclient
+#include "mpack.h"         // https://ludocode.github.io/mpack/
 
 class LumaVibe {
   public: 
@@ -22,7 +23,10 @@ class LumaVibe {
       ERROR_NOT_ENOUGH_MEMORY  = 30,
       ERROR_MODEM_RESTART_FAIL = 40,
       ERROR_MODEM_NETWORK      = 41,
-      ERROR_MODEM_GPRS = 42,
+      ERROR_MODEM_GPRS         = 42,
+      ERROR_MQTT               = 43,
+      ERROR_PUBLISH_BEGIN_FAIL = 50,
+      ERROR_PUBLISH_END_FAIL   = 51,
       ERROR_SENSOR_INIT        = 100
     };
 
@@ -33,6 +37,7 @@ class LumaVibe {
       char            publishTopic[20];
       char            subscribeTopic[20];
       char            format[10]; // always set to "Int16" for now, remove completely later? any use?
+      uint8_t         msgType;
       uint16_t        frequency; // What should frequency actually mean?
       uint16_t        samplesPerMeasurement;
       uint16_t        measurementInterval_ms;
@@ -50,23 +55,31 @@ class LumaVibe {
 
     LumaVibe();
     ERROR init(Parameters_t params);
-    ERROR begin(uint16_t transientThreshold, uint16_t transientDebounceCntr);
+    ERROR begin();
     ERROR measure();
+    ERROR packData(uint32_t *bytesPacked);
+    ERROR publishData(uint32_t bytesToPublish, uint16_t bytesPerWrite);
+    void  getCommandsFromServer(MQTT_CALLBACK_SIGNATURE);
+    void  clearAccelInterrupt();
+    void dumpSimInfo();
 
   private: 
-    Parameters_t _params;
-
-    MMA8451Q _accel;
-
-    TinyGsm       _modem;
-    TinyGsmClient _client;
-    PubSubClient  _mqtt;
+    Parameters_t   _params;
+    MMA8451Q       _accel;
+    TinyGsm        _modem;
+    TinyGsmClient  _client;
+    PubSubClient   _mqtt;
+    mpack_writer_t _writer;
+    char           *_packBuffer;
 
     struct {
-      struct {
-        int16_t xi;
-        int16_t yi;
-        int16_t zi;
+      union {
+        int16_t v[3];
+        struct {
+          int16_t xi;
+          int16_t yi;
+          int16_t zi;
+        };
       } *data;
       bool isBufferAllocated;
     } _accelBuffer;
@@ -79,9 +92,11 @@ class LumaVibe {
     } _timers;
 
     ERROR enableTimer(hw_timer_t *timer, uint8_t timerNumber, uint64_t timer_ms, void (*timerISR)(void));
-    void keepAlive();
-    void clearMeasurementData();
+    void  keepAlive();
+    void  clearMeasurementData();
     ERROR setupModem();
+    ERROR getTimestampFromNetwork(String &timeStamp);
+    void  packArray(mpack_writer_t *writer, const char *entryNames[3], const uint16_t length);
 };
 
 #endif //LUMAVIBE_H
