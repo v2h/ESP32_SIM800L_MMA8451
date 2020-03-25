89d22def21bdb1c615601183b28c827c286ed549
Hung Vu <viethungvu@hotmail.com>
2020-03-25 01:35:47 +0100

    Add power boost function for TTGO board

--- LumaVibe.h
+++ LumaVibe.h
@@ -13,6 +13,28 @@
 #include "PubSubClient.h"  // https://github.com/knolleary/pubsubclient
 #include "mpack.h"         // https://ludocode.github.io/mpack/
 
+// TODO: move this thing somewhere else
+#ifdef TTGO
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
+bool setPowerBoostKeepOn(bool en)
+{
+  Wire.beginTransmission(IP5306_ADDR);
+  Wire.write(IP5306_REG_SYS_CTL0);
+  if (en) {
+    Wire.write(0x37); // Set bit1: 1 enable 0 disable boost keep on
+  } else {
+    Wire.write(0x35); // 0x37 is default reg value
+  }
+  return Wire.endTransmission() == 0;
+}
+#endif // TTGO
+
+// Marcro for handdling errors
+#if LUMAVIBE_ENABLE_HANDLING_ENABLE
+#define HANDLE_ERROR(e) handleError(e, __LINE__)
+#endif // LUMAVIBE_ENABLE_HANDLING_ENABLE
+
 class LumaVibe {
   public: 
     enum ERROR {
